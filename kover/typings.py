88d6fb2519f18187981a04f19c30ea519309b611
Maksim Guzij <gunter56741@gmail.com>
2024-10-20 13:41:00 +0300

    removed TypeAlias

--- kover/typings.py
+++ kover/typings.py
@@ -1,7 +1,7 @@
-from typing import Dict, Any, Union, Literal, TypeAlias, List
+from typing import Dict, Any, Union, Literal, List
 
 from bson import SON
 
-xJsonT: TypeAlias = Dict[str, Any]
-DocumentT: TypeAlias = Union[xJsonT, SON]
-COMPRESSION_T: TypeAlias = List[Literal["zlib", "zstd", "snappy"]]
+xJsonT = Dict[str, Any]
+DocumentT = Union[xJsonT, SON]
+COMPRESSION_T = List[Literal["zlib", "zstd", "snappy"]]
