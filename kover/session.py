6c22103f0fb96cc9fae37936359d94db49de646e
Maksim Guzij <gunter56741@gmail.com>
2024-10-20 13:59:48 +0300

    fix Self v2

--- kover/session.py
+++ kover/session.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import time
 from enum import Enum
-from typing import TYPE_CHECKING, TypeVar, Optional, List, Self, Literal, Type
+from typing import TYPE_CHECKING, TypeVar, Optional, List, Literal, Type
 from types import TracebackType
 
 from bson import Int64, Binary
