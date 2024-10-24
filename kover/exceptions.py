1ceac3e270edd638626ea3200182d8f4bd78390f
Maksim Guzij <gunter56741@gmail.com>
2024-10-24 23:37:32 +0300

    removed __str__ and __repr__ from OperationFailure

--- kover/exceptions.py
+++ kover/exceptions.py
@@ -4,8 +4,3 @@ class OperationFailure(Exception):
     def __init__(self, code: int, message: Any) -> None:
         self.code = code
         self.message = message
-    
-    def __repr__(self) -> str:
-        return str(self.message)
-
-    __str__ = __repr__
