76046be65ca4d27ae71389b99b5b78dd34e4721d
Maksim Guzij <gunter56741@gmail.com>
2024-11-12 23:59:12 +0300

    bump version and changed setup.py a bit

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
-from kover import __version__
-
+import sys
+from pathlib import Path
+import pkg_resources
 from setuptools import setup  # type: ignore
+from typing import Dict, Any
 
-setup(
-    name="kover",
-    version=__version__,
-    install_requires=["pymongo", "attrs", "typing-extensions"],
-    packages=['kover'],
-    description="fully async mongodb driver for mongod and replica sets",
-    author="oMegaPB",
-    url="https://github.com/oMegaPB/kover"
-)
+import kover
+from kover import __version__, __author__
+
+if sys.version_info < (3, 10):
+    raise Exception(f"Unsupported python version: {sys.version_info}")
+
+requirements = Path.cwd() / "requirements.txt"
+install_requires = [
+    req.name
+    for req in pkg_resources.parse_requirements(requirements.open())
+]
+
+kwargs: Dict[str, Any] = {
+    "name": kover.__name__,
+    "version": __version__,
+    "install_requires": install_requires,
+    "packages": [kover.__name__],
+    "description": "fully async mongodb driver for mongod and replica sets",
+    "author": __author__,
+    "url": "https://github.com/oMegaPB/kover"
+}
+
+setup(**kwargs)
